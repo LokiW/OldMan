ff0d95600ac5808ad276aa695e92e63b30d8f5a4
Davis Yoshida <davis.yoshida@gmail.com>
2014-08-13 21:12:31 -0700

    Added constants for moves. Removed Main class. Added OldMan class.

---
+++ src/standoff/oldman/Constants.java
@@ -0,0 +1,14 @@
+package standoff.oldman;
+
+
+public final class Constants {
+    public static final String MOVE_SHOOT = "S";
+
+    public static final String MOVE_DODGE = "D";
+
+    public static final String MOVE_PREPARE = "P";
+
+    public static final String MOVE_THROW = "T";
+
+    public static final String MOVE_NOTHING = "N";
+}
---
+++ src/standoff/oldman/OldMan.java
@@ -0,0 +1,7 @@
+package standoff.oldman;
+
+public class OldMan {
+    public String randomMove(){
+        return "4"; //Chosen by fair dice roll, guaranteed to be randoms
+    }
+}
