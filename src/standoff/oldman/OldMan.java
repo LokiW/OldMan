2606abe938de24f76df7dc00766229d98ebb394d
LokiW <loki.w.white@gmail.com>
2014-08-14 18:34:52 -0700

    Loki, filled in random move

--- src/standoff/oldman/OldMan.java
+++ src/standoff/oldman/OldMan.java
@@ -1,7 +1,27 @@
 package standoff.oldman;
+import java.util.Random
 
 public class OldMan {
     public String randomMove(){
-        return "4"; //Chosen by fair dice roll, guaranteed to be randoms
+	Random rand = new Random();
+	int actionNum = rand.nextInt(5);
+	String action = "";
+	int target = rand.nextInt(4);
+
+	switch (action) {
+	    case 0: 
+		action = Constants.MOVE_SHOOT + " " + target;
+	    case 1:
+		action = Constants.MOVE_DODGE + " " + target;
+	    case 2:
+		action = Constants.MOVE_PREPARE;
+	    case 3:
+		action = Constants.MOVE_THROW + " " + target;
+	    case 4:
+		action = Constants.MOVE_NOTHING;
+	}
+        return action; //Chosen by fair dice roll, guaranteed to be randoms
     }
+
+
 }
